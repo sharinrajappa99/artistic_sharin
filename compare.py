eab052e31e38be4bb0c8764ab4ef8ceb85848cee
sharinrajappa99 <42990314+sharinrajappa99@users.noreply.github.com>
2018-09-19 10:20:04 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,10 @@
+#to compare two numbers
+a=int(input("the no 'a'"))
+b=int(input("the no 'b'"))
+if(a>b):
+	print("a' is greater than 'b'")
+else:
+	if(b>a):
+		print("b' is greater than 'a'")
+	else:
+		print("the nos are equal")
