81dc57b3664ea23fbc256556852e5a5d98c7e790
sharinrajappa99 <42990314+sharinrajappa99@users.noreply.github.com>
2018-09-19 09:56:35 +0530

    Add files via upload

--- rollthedice.py
+++ rollthedice.py
@@ -1,3 +1,11 @@
 # input from the user to roll the dice
-a=str(input("enter r to roll and q to quit"))
 i=0
+import random 
+while i<5:
+	a=str(input("enter r to roll the dice and q to quit"))
+	if(a=='r'):
+		print(random.randint(1,6))
+	else:
+		print("exit")
+		break
+    
